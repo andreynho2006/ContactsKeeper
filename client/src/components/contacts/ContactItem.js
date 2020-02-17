9ac85da5a73085a26e21968876b927c7da2e442e
andreynho2006 <andreynho2006@gmail.com>
2020-02-17 18:06:48 +0200

    Make the type of the contact display with first letter upperCase

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -8,10 +8,12 @@ const ContactItem = ({ contact }) => {
         {name}{' '}
         <span
           className={
-            'badge' +
+            'badge ' +
             (type === 'professional' ? 'badge-success' : 'badge-primary')
           }
-        ></span>
+        >
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </span>
       </h3>
     </div>
   );
