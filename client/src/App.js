a7e2c455ab299a3127458e126aa1cbf0c59cee58
andreynho2006 <andreynho2006@gmail.com>
2020-02-18 19:24:06 +0200

    add Register to routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
 import ContactState from './context/contact/ContactState';
@@ -18,6 +19,7 @@ const App = () => {
               <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
               </Switch>
             </div>
           </Fragment>
