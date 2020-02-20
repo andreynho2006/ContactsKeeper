e141c1b682577e4bc942aa530f058cf9cdbd83af
andreynho2006 <andreynho2006@gmail.com>
2020-02-20 13:29:56 +0200

    make / a protected route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layouts/Alerts';
+import PrivateRoute from './components/routing/PrivatesRoutes';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
 import ContactState from './context/contact/ContactState';
@@ -27,7 +28,7 @@ const App = () => {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
